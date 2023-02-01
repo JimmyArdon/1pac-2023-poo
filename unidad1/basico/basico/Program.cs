9fa521055e638bc814c38c0776ad492f1fac6888
JimmyArdon <jimmyardonorellana@gmail.com>
2023-01-31 18:07:45 -0600

    Agregar visual Code

--- unidad1/basico/basico/Program.cs
+++ unidad1/basico/basico/Program.cs
@@ -11,7 +11,11 @@ namespace basico
     {
         static void Main(string[] args)
         {
-            Tipos01 t01 = new Tipos01();
+            Tipos01 t01 = new Tipos01("Hola buenas noches");
+            Tipos01 t02 = new Tipos01("Hola buenas tardes");
+            Tipos01 t03 = new Tipos01("Hola buenos dias");
+            Tipos01 t04 = new Tipos01("Hola feliz cumpleaños");
+            Tipos01 t05 = new Tipos01("Hola");
         }
     }
 }
